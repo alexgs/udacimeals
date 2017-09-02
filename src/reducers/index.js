0077b75315945865918ae98e49c9d092e39940db
Phil Gates-Shannon <phil.gs@gmail.com>
2017-09-02 12:54:35 -0400

    Add reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,62 @@
+import { ADD_RECIPE, REMOVE_FROM_CALENDAR } from '../actions';
+
+const initialCalendarState = {
+    monday: {
+        breakfast: null,
+        lunch: null,
+        dinner: null
+    },
+    tuesday: {
+        breakfast: null,
+        lunch: null,
+        dinner: null
+    },
+    wednesday: {
+        breakfast: null,
+        lunch: null,
+        dinner: null
+    },
+    thursday: {
+        breakfast: null,
+        lunch: null,
+        dinner: null
+    },
+    friday: {
+        breakfast: null,
+        lunch: null,
+        dinner: null
+    },
+    saturday: {
+        breakfast: null,
+        lunch: null,
+        dinner: null
+    },
+    sunday: {
+        breakfast: null,
+        lunch: null,
+        dinner: null
+    }
+};
+
+export const calendarReducer = function( state=initialCalendarState, action ) {
+    switch ( action.type ) {
+        case ADD_RECIPE:
+            return {
+                ...state,
+                [ action.day ]: {
+                    ...state[ action.day ],
+                    [ action.meal ]: action.recipe
+                }
+            };
+        case REMOVE_FROM_CALENDAR:
+            return {
+                ...state,
+                [ action.day ]: {
+                    ...state[ action.day ],
+                    [ action.meal ]: null
+                }
+            };
+        default:
+            return state;
+    }
+};
