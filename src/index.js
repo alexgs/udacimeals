c745427c3c6d9c97cdd5b8ebeed268d6d255f1ce
Phil Gates-Shannon <phil.gs@gmail.com>
2017-09-02 13:01:34 -0400

    Create Redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore } from 'redux';
+import App from './components/App';
+import { calendarReducer } from './reducers';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+const store = createStore( calendarReducer );
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
