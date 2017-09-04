31e82be832159f719e2a76466747d554c69785af
Phil Gates-Shannon <phil.gs@gmail.com>
2017-09-04 14:38:43 -0400

    Use "connect" function to link App component and Redux store (3.4)

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addRecipe, removeFromCalendar } from '../actions';
 import '../App.css';
 
 class App extends Component {
@@ -9,4 +11,17 @@ class App extends Component {
     }
 }
 
-export default App;
+function mapStateToProps( state ) {
+    return {
+        calendar: state
+    };
+}
+
+function mapDispatchToProps( dispatch ) {
+    return {
+        addRecipeToCalendar: ( day, recipe, meal ) => dispatch( addRecipe( { day, recipe, meal } ) ),
+        removeRecipeFromCalendar: ( day, meal ) => dispatch( removeFromCalendar( { day, meal } ) )
+    };
+}
+
+export default connect( mapStateToProps, mapDispatchToProps )( App );
