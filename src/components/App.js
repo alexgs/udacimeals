f5efa53c57a083d8d40ba9f49ee37821f17af04a
Phil Gates-Shannon <phil.gs@gmail.com>
2017-09-08 13:26:09 -0400

    Add calendar grid to App render method (4.4.A)

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,67 @@
 import React, { Component } from 'react';
+import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o';
 import { connect } from 'react-redux';
 import { addRecipe, removeFromCalendar } from '../actions';
 import '../App.css';
+import { capitalize } from '../utils/helpers'
 
 class App extends Component {
     render() {
+        const { calendar, addRecipe, removeRecipe } = this.props;
+        const mealOrder = [ 'breakfast', 'lunch', 'dinner' ];
+
+
         return (
-            <p>Hello World</p>
+            <div className="container">
+                <ul className="meal-types">
+                    { mealOrder.map( mealName =>
+                        <li key={ mealName } className="subheader">
+                            { capitalize( mealName ) }
+                        </li>
+                    ) }
+                </ul>
+
+                <div className="calendar">
+                    <div className='days'>
+                        { calendar.map( ({ day }) =>
+                            <h3 key={day} className='subheader'>
+                                { capitalize(day) }
+                            </h3>
+                        ) }
+                    </div>
+                    <div className='icon-grid'>
+                        { calendar.map( ( calendarData ) => {
+                            const today = calendarData.day;
+                            const todaysMeals = calendarData.meals;
+                            return (
+                                <ul key={today}>
+                                    { mealOrder.map( mealName =>
+                                        <li key={ mealName } className="meal">
+                                            { todaysMeals[mealName]
+                                                ? <div className='food-item'>
+                                                    <img
+                                                        src={todaysMeals[mealName].image}
+                                                        alt={todaysMeals[mealName].label}
+                                                    />
+                                                    <button onClick={ () => removeRecipe( {
+                                                        meal: mealName,
+                                                        day:today
+                                                    } ) }>
+                                                        Clear
+                                                    </button>
+                                                </div>
+                                                : <button className='icon-btn'>
+                                                    <CalendarIcon size={30}/>
+                                                </button>
+                                            }
+                                        </li>
+                                    ) }
+                                </ul>
+                            );
+                        } ) }
+                    </div>
+                </div>
+            </div>
         );
     }
 }
