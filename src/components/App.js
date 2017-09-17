af9527bc5c3c95f46935b37515be5f4534a39f40
Phil Gates-Shannon <phil.gs@gmail.com>
2017-09-17 08:43:55 -0400

    Add functionality to add and remove recipes to/from calendar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,64 @@
 import React, { Component } from 'react';
+import ArrowRightIcon from 'react-icons/lib/fa/arrow-circle-right';
 import CalendarIcon from 'react-icons/lib/fa/calendar-plus-o';
+import Loading from 'react-loading';
+import Modal from 'react-modal';
 import { connect } from 'react-redux';
 import { addRecipe, removeFromCalendar } from '../actions';
-import { capitalize } from '../utils/helpers'
+import { fetchRecipes } from '../utils/api';
+import { capitalize } from '../utils/helpers';
+import FoodList from "./FoodList";
 
 class App extends Component {
+    constructor( props ) {
+        super( props );
+        this.state = {
+            foodModalOpen: false,
+            meal: null,
+            day: null,
+            food: null,
+        };
+        this.closeFoodModal = this.closeFoodModal.bind( this );
+        this.openFoodModal = this.openFoodModal.bind( this );
+        this.searchFood = this.searchFood.bind( this );
+    }
+
+    openFoodModal( { meal, day } ) {
+        this.setState( {
+            foodModalOpen: true,
+            meal,
+            day
+        } );
+    }
+
+    closeFoodModal() {
+        this.setState( {
+            foodModalOpen: false,
+            meal: null,
+            day: null,
+            food: null,
+        } );
+    }
+
+    searchFood( event ) {
+        if ( !this.input.value) {
+            return undefined;
+        }
+        event.preventDefault();
+        this.setState( { loadingFood: true } );
+
+        fetchRecipes( this.input.value )
+            .then( food => this.setState( {
+                food,
+                loadingFood: false
+            } ) );
+    }
+
     render() {
-        const { calendar, addRecipe, removeRecipe } = this.props;
+        const { foodModalOpen, loadingFood, food } = this.state;
+        const calendar = this.props.calendar;
+        const addRecipe = this.props.addRecipeToCalendar;
+        const removeRecipe = this.props.removeRecipeFromCalendar;
         const mealOrder = [ 'breakfast', 'lunch', 'dinner' ];
 
 
@@ -42,14 +94,20 @@ class App extends Component {
                                                         src={todaysMeals[mealName].image}
                                                         alt={todaysMeals[mealName].label}
                                                     />
-                                                    <button onClick={ () => removeRecipe( {
+                                                    <button onClick={() => removeRecipe( {
                                                         meal: mealName,
-                                                        day:today
-                                                    } ) }>
+                                                        day: today
+                                                    } )}>
                                                         Clear
                                                     </button>
                                                 </div>
-                                                : <button className='icon-btn'>
+                                                : <button
+                                                    className='icon-btn'
+                                                    onClick={() => this.openFoodModal( {
+                                                        meal: mealName,
+                                                        day:today
+                                                    } )}
+                                                >
                                                     <CalendarIcon size={30}/>
                                                 </button>
                                             }
@@ -60,6 +118,48 @@ class App extends Component {
                         } ) }
                     </div>
                 </div>
+
+                <Modal
+                    className="modal"
+                    overlayClassName="overlay"
+                    isOpen={ foodModalOpen }
+                    onRequestClose={ this.closeFoodModal }
+                    contentLabel="Modal"
+                >
+                    <div>
+                        { loadingFood === true
+                            ? <Loading delay={200} type='spin' color='#222' className='loading' />
+                            : <div className='search-container'>
+                                <h3 className='subheader'>
+                                    Find a meal for {capitalize(this.state.day)} {this.state.meal}.
+                                </h3>
+                                <div className='search'>
+                                    <input
+                                        className='food-input'
+                                        type='text'
+                                        placeholder='Search Foods'
+                                        ref={(input) => this.input = input}
+                                    />
+                                    <button
+                                        className='icon-btn'
+                                        onClick={this.searchFood}>
+                                        <ArrowRightIcon size={30}/>
+                                    </button>
+                                </div>
+                                {food !== null && (
+                                    <FoodList
+                                        food={food}
+                                        onSelect={(recipe) => {
+                                            // console.log( JSON.stringify( recipe, null, 2 ) );
+                                            addRecipe({ recipe, day: this.state.day, meal: this.state.meal });
+                                            this.closeFoodModal();
+                                        }}
+                                    />
+                                )}
+                            </div>
+                        }
+                    </div>
+                </Modal>
             </div>
         );
     }
@@ -80,8 +180,8 @@ function mapStateToProps( { calendar, food } ) {
 
 function mapDispatchToProps( dispatch ) {
     return {
-        addRecipeToCalendar: ( day, recipe, meal ) => dispatch( addRecipe( { day, recipe, meal } ) ),
-        removeRecipeFromCalendar: ( day, meal ) => dispatch( removeFromCalendar( { day, meal } ) )
+        addRecipeToCalendar: ( { day, recipe, meal } ) => dispatch( addRecipe( { day, recipe, meal } ) ),
+        removeRecipeFromCalendar: ( { day, meal } ) => dispatch( removeFromCalendar( { day, meal } ) )
     };
 }
 
